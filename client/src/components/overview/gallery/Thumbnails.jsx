aae69f56b554ddd9bd32877ea67669fa3fa5eb6e
Tea Skela <dskela@jd20.law.harvard.edu>
2021-06-19 14:54:57 -0700

    image resizing api

--- client/src/components/overview/gallery/Thumbnails.jsx
+++ client/src/components/overview/gallery/Thumbnails.jsx
@@ -2,15 +2,18 @@ import React, { useEffect, useState } from "react";
 import style from "../MainOverview.module.css";
 
 const Thumbnails = ({ thumbUrl, handleThumb }) => {
-  
-
   return (
-    <div id={style.thumbImage} style={{background: thumbUrl ? `url('${thumbUrl}') center / cover` : null}}
+    <div
+      id={style.thumbImage}
+      style={{
+        background: thumbUrl
+          ? `url('${thumbUrl}') center / cover`
+          : null,
+      }}
       onClick={(e) => {
         handleThumb(e, thumbUrl);
       }}
-    >
-    </div>
+    ></div>
   );
 };
 
